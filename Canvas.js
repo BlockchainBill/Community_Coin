f1206dfad1a411a26361c124a2916b0ff1f52bb6
blockchainbill <blockchainbill@MSI.localdomain>
2022-08-26 02:02:47 -0400

    added canvas

---
+++ Canvas.js
@@ -0,0 +1,43 @@
+const { deflateRaw } = require("zlib");
+
+//Canvas Settings
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+
+canvas.height = windowHeight * 0.5;
+canvas.width = windowWidth * 0.5;
+
+canvas.style.background = #ff8;
+
+class Circle {
+    constructor(xpos, ypos, radius, color) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw(context)  {
+        context.beginPath();
+        context.arc(this.xpos, this.ypos, this.radius, 0, MATH.PI * 2, false);
+        context.stroke();
+        context.closePath();
+    }
+}
+
+let all_circles = [];
+
+let createCircle = function(circle) {
+    circle.draw(context);
+}
+
+for ( var numbers = 0; numbers <6; numbers++ ) {
+    let random_x = Math.random() * (windowWidth * 0.5);
+    let random_y = Math.random() * (windowHeight * 0.5);
+
+    let my_circle = new Circle(random_x, random_y, 50, "black");
+    all_circles.push(my_circle);
+    createCircle(all_circles[numbers]);
+}
